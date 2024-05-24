6e4eb37b258ef8465c3b8712f2a2a128c6ceb376
doboalegy <pongonretwork@gmail.com>
2024-05-24 15:28:45 +0200

    Palindrome Checker

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,6 +1,5 @@
 import re
 
-# with open('./input.txt', 'r') as f:
 with open('./input.txt', 'r') as f:
     input = f.read()
 
@@ -11,10 +10,13 @@ for expression in expressions:
     # Removing non alphanumerical characters
     expression =  re.sub(r'\W+', '', expression)
 
+    # Getting the number of alphanumerical characters
+    num = len(set(expression))
+
     # Uppercasing expression
     expression = expression.upper()
 
-    if expression and (expression == expression[::-1]):
-        print(f'YES, {len(set(expression))}')
+    if expression and expression == expression[::-1]:
+        print(f'YES, {num}')
     else:
         print('NO, -1')
