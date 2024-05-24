d09a59a6446b0571175fc2e969cfbc49aaefc528
doboalegy <pongonretwork@gmail.com>
2024-05-24 15:30:22 +0200

    Palindrome

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -10,13 +10,10 @@ for expression in expressions:
     # Removing non alphanumerical characters
     expression =  re.sub(r'\W+', '', expression)
 
-    # Getting the number of alphanumerical characters
-    num = len(set(expression))
-
     # Uppercasing expression
     expression = expression.upper()
 
     if expression and expression == expression[::-1]:
-        print(f'YES, {num}')
+        print(f'YES, {len(set(expression))}')
     else:
         print('NO, -1')
