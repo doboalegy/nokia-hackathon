caf9e38ab1339cf6964a2d35c0513226efaab58f
doboalegy <pongonretwork@gmail.com>
2024-05-24 15:52:57 +0200

    palindrome

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -8,7 +8,7 @@ expressions = input.split('\n')
 
 for expression in expressions:
     # Removing non alphanumerical characters and making text lowercase
-    expression =  re.sub(r'\W+', '', expression).lower()
+    expression =  re.sub(r'[^a-zA-Z0-9]', '', expression).lower()
 
     if expression and expression == expression[::-1]:
         print(f'YES, {len(set(expression))}')
