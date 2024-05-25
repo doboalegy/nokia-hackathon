46d600ddde5cb61e0b3887551fc2d0da62a8e14d
doboalegy <pongonretwork@gmail.com>
2024-05-25 15:10:01 +0200

    maze matrix

--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -1,4 +1,36 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+# Reading input
+with open('./matrix_operations/input.txt', 'r') as f:
+	input = f.read().split('operations')
 
-print(input)
+# Storing matrixes
+matrixes = {}
+
+key = ''
+matrix = []
+
+for row in input[0].split('\n'):
+	row_list = row.strip().split()
+
+	if not row_list:
+		continue
+
+	if len(row_list) == 1:
+		if key:
+			matrixes[key] = matrix
+		key = row_list[0]
+		matrix = []
+	else:
+		matrix.append(row_list)
+
+if key:
+	matrixes[key] = matrix
+
+# Storing operations
+operations = []
+for row in input[1].split('\n'):
+	if not row:
+		continue
+	
+	operations.append(row)
+
+print(operations)
--- maze_solver/main.py
+++ maze_solver/main.py
@@ -70,7 +70,73 @@ for key, maze in mazes.items():
         
     print()
 '''
-with open('./input.txt', 'r') as f:
-    input = f.read()
 
-print(input)
+from collections import deque
+
+def find_shortest_path(maze):
+    # Segédfüggvény a szomszédos cellák meghatározására
+    def get_neighbors(x, y):
+        for dx, dy, direction in [(-1, 0, 'U'), (1, 0, 'D'), (0, -1, 'L'), (0, 1, 'R')]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(maze) and 0 <= ny < len(maze[0]) and maze[nx][ny] != '#':
+                yield nx, ny, direction
+    
+    # Keresd meg a start és cél koordinátáit
+    start = goal = None
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == 'S':
+                start = (i, j)
+            elif maze[i][j] == 'G':
+                goal = (i, j)
+    
+    if not start or not goal:
+        return "Start or goal not found in the maze."
+    
+    # BFS kezdeményezése
+    queue = deque([(start[0], start[1], "")])
+    visited = set()
+    visited.add((start[0], start[1]))
+    
+    while queue:
+        x, y, path = queue.popleft()
+        
+        # Ha elérte a célt, visszatérítjük az utat
+        if (x, y) == goal:
+            return 'S' + path + 'G'
+        
+        # Szomszédos cellák bejárása
+        for nx, ny, direction in get_neighbors(x, y):
+            if (nx, ny) not in visited:
+                visited.add((nx, ny))
+                queue.append((nx, ny, path + direction))
+    
+    return "No path found from start to goal."
+
+# Reading mazes from file
+mazes = {}
+with open('./input.txt', 'r') as f:
+    key = ''
+    matrix = []
+
+    for row in f:
+        row_list = row.strip().split()
+
+        if not row_list:
+            continue
+
+        if len(row_list) == 1:
+            if key:
+                mazes[key] = matrix
+            key = row_list[0]
+            matrix = []
+        else:
+            matrix.append(row_list)
+    
+    if key:
+        mazes[key] = matrix
+
+for key, maze in mazes.items():
+    print(key)
+    print(find_shortest_path(maze))
+    print()
