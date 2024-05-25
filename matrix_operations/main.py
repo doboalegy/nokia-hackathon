3d144d22960dc23256170b851c4d17c5fa030fb7
doboalegy <pongonretwork@gmail.com>
2024-05-25 17:54:20 +0200

    matrix

--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -1,36 +1,83 @@
-# Reading input
-with open('./input.txt', 'r') as f:
-	input = f.read().split('operations')
+import re
 
-# Storing matrixes
-matrixes = {}
+def print_matrix(matrix : list):
+	for row in matrix:
+		print(' '.join(map(str, row)))
 
-key = ''
-matrix = []
+def add_matrices(matrix1 : list, matrix2 : list) -> list:
+	matrix = []
+	for i in range(len(matrix1)):
+		matrix.append([])
+		for j in range(len(matrix1[0])):
+			matrix[i].append([])
+			matrix[i][j] = matrix1[i][j] + matrix2[i][j]
+	return matrix
 
-for row in input[0].split('\n'):
-	row_list = row.strip().split()
 
-	if not row_list:
-		continue
+with open('./input.txt') as f:
+	matrices = {}
+	operations = []
 
-	if len(row_list) == 1:
-		if key:
-			matrixes[key] = matrix
-		key = row_list[0]
-		matrix = []
-	else:
-		matrix.append(row_list)
+	key = ''
+	matrix = []
 
-if key:
-	matrixes[key] = matrix
+	''' READING MATRIXES '''
+	line = f.readline()
+	while line:
+		# Stripping line
+		line = line.strip()
+		
+		if len(line) == 0: # empty line
+			pass
+		elif len(line) == 1: # new matrix
+			if matrix:
+				matrices[key] = matrix
+				matrix = []
+			key = line
+		elif line == 'operations': # starting operations
+			matrices[key] = matrix
+			break
+		elif all(c.isdigit() or c.isspace() for c in line): # If line contains digits and spaces
+			matrix.append(list(map(int, line.split())))
 
-# Storing operations
-operations = []
-for row in input[1].split('\n'):
-	if not row:
-		continue
+		# Reading next line
+		line = f.readline()
 	
-	operations.append(row)
+	''' READING OPERATIONS '''
+	line = f.readline()
+	while line:
+		# Stripping line
+		line = line.strip()
 
-print(operations)
+		# Adding line to operations
+		if len(line) > 0:
+			operations.append(line)
+
+		# Reading next line
+		line = f.readline()
+
+''' MAIN '''
+i = 0 # used to name new matrices
+
+for operation in operations:
+	# Printing operation
+	print(operation)
+
+	# Additions
+	additions = re.findall(r'\w+\s\+\s\w+', operation)
+	while additions:
+
+		matrix_name = 'M' + str(i)
+		addends = additions[0].split(' + ')
+		matrices[matrix_name] = add_matrices(matrices[addends[0]], matrices[addends[1]])
+		
+		# Replacing operation with new matrix
+		operation = operation.replace(additions[0], matrix_name)
+
+		i += 1
+		additions = re.findall(r'\w+\s\+\s\w+', operation)
+	
+	print_matrix(matrices['M' + str(i-1)])
+
+	# Empty line
+	print()
