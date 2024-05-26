e0706599f8bd646918404d6901491bb68716dbe1
doboalegy <pongonretwork@gmail.com>
2024-05-26 20:06:30 +0200

    palindrome

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -3,14 +3,19 @@ import re
 with open('./input.txt', 'r') as f:
     input = f.read()
 
-# Storing expressions in a list
+# Kifejezések tárolása listában
 expressions = input.split('\n')
 
 for expression in expressions:
-    # Removing non alphanumerical characters and making text lowercase
+
+    # Üres kifejezéseket kihagyjuk
+    if not expression:
+        continue
+
+    # Nem alfanumerikus karakterek elhagyása, kisbetüssé alakítás
     expression =  re.sub(r'[^a-zA-Z0-9]', '', expression).lower()
 
-    if expression and expression == expression[::-1]:
+    if expression == expression[::-1]:
         print(f'YES, {len(set(expression))}')
     else:
         print('NO, -1')
