b8562636c9c4400a82959c2b8856ca76a1b0a2e6
doboalegy <pongonretwork@gmail.com>
2024-05-27 18:21:57 +0200

    maze

--- maze_solver/main.py
+++ maze_solver/main.py
@@ -1,57 +1,52 @@
 from collections import deque
-        
+
 class Maze:
-    def __init__(self, matrix):
-        self.cells = {}
+    def __init__(self, cells):
+        self.cells = cells
         self.start = None
         self.goal = None
 
-        # Cellák hozzáadása
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                type = matrix[i][j]
-                self.cells[i, j] = type
-                if type == 'S':
+        # Start és cél megkeresése
+        for i in range(len(self.cells)):
+            for j in range(len(self.cells[i])):
+                if self.cells[i][j] == 'S':
                     self.start = (i, j)
-                if type == 'G':
+                elif self.cells[i][j] == 'G':
                     self.goal = (i, j)
-        
-    def find_shortest_path(self):
-        # Szomszédos cellák meghatározása
-        def get_neighbors(cell):
-            neighbours = set()
-            for dx, dy, direction in [(-1, 0, ' U'), (1, 0, ' D'), (0, -1, ' L'), (0, 1, ' R')]:
-                x, y = cell[0] + dx, cell[1] + dy
-                if ( (x, y) in self.cells.keys() ) and ( self.cells[(x, y)] != '#' ):
-                    neighbours.add( ((x, y), direction) )
-            return neighbours
 
-        # Start és Cél ellenörzése
-        if not self.start or not self.goal:
-            raise Exception('Maze is missing start or goal')
-    
+    def find_best_path(self):
+        # Cella szomszédjainak megadása
+        def get_neighbors(x, y):
+            for dx, dy, direction in [(-1, 0, ' U'), (1, 0, ' D'), (0, -1, ' L'), (0, 1, ' R')]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < len(self.cells) and 0 <= ny < len(self.cells[0]) and self.cells[nx][ny] != '#':
+                    yield nx, ny, direction
+
+        # Start és cél ellenörzése
+        if self.start is None or self.goal is None:
+            raise Exception('Start or goal is not set')
+
         # Legjobb út megkeresése BFS algoritmussal
-        queue = deque([(self.start, "")])
-        visited = set()
-        visited.add((self.start))
-        
+        queue = deque([(self.start[0], self.start[1], "")])
+        explored = set()
+        explored.add((self.start[0], self.start[1]))
+
         while queue:
-            current_cell, path = queue.popleft()
-            
-            # Ha elértük a célt, visszadjuk az utat
-            if current_cell == self.goal:
+            x, y, path = queue.popleft()
+
+            # Ha megtaláltuk a célt
+            if (x, y) == self.goal:
                 return 'S' + path + ' G'
             
-            # Szomszédos cellák bejárása
-            for neighbour, direction in get_neighbors(current_cell):
-                if neighbour not in visited:
-                    visited.add(neighbour)
-                    queue.append((neighbour, path + direction))
-        
-        raise Exception('Maze has no solution')
+            # Szomszédos cellák felfedezése
+            for nx, ny, direction in get_neighbors(x, y):
+                if (nx, ny) not in explored:
+                    explored.add((nx, ny))
+                    queue.append((nx, ny, path + direction))
 
-''' INPUT '''
-# Az útvesztöket ebben a szótárban tároljuk
+        raise Exception("Maze has no solution")
+
+# Store mazes in this dictionary
 mazes = {}
 
 with open('./maze_solver/input.txt', 'r') as f:
@@ -61,23 +56,24 @@ with open('./maze_solver/input.txt', 'r') as f:
     for line in f:
         row = line.strip().split()
 
-        #Üres sort kihagyjuk
+        # Skip empty lines
         if not row:
             continue
 
-        if len(row) == 1: # Új útvesztö
+        if len(row) == 1: # New maze
             if key:
                 mazes[key] = Maze(matrix)
             key = row[0]
             matrix = []
-        else: # Jelenlegi útvesztö bövítése
+        else: # Expand current maze
             matrix.append(row)
-    
-    # Utolsó útvesztö hozzáadása
-    mazes[key] = Maze(matrix)
+
+    # Add the last maze
+    if key:
+        mazes[key] = Maze(matrix)
 
 ''' MAIN '''
 for key, maze in mazes.items():
     print(key)
-    print(maze.find_shortest_path())
+    print(maze.find_best_path())
     print()
